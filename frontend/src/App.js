c8c51f616266565e20f9b3d73823d938c0ffb8a9
“tanishq20” <tanishqkumarsgr98@gmail.com>
2022-08-14 19:12:26 +0530

    feat: added frontend and connected with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import PrivateRoute from './routing/PrivateRoute'
+import NotFound from './components/NotFound'
+import Dashboard from './components/Dashboard'
+import setAuthToken from './utils/setAuthToken'
+import store from './store'
+import { loadUser } from './actions/auth'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
 }
 
-export default App;
+function App() {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+  return (
+    <>
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='' component={NotFound} />
+          </Switch>
+        </Router>
+      </div>
+    </>
+  )
+}
+
+export default App
